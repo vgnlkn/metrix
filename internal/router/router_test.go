40f8590605386255c473290ad30ed32660ad582e
vgnlkn <vgn1lkn26@gmail.com>
2025-03-10 23:59:58 +0300

    fixed resp body must be closed

--- internal/router/router_test.go
+++ internal/router/router_test.go
@@ -26,42 +26,42 @@ func TestRouter(t *testing.T) {
 	ts := httptest.NewServer(router.Mux)
 	defer ts.Close()
 
-	testRequest := func(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
+	testRequest := func(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (int, string) {
 		req, err := http.NewRequest(method, ts.URL+path, body)
 		if err != nil {
 			t.Fatal(err)
-			return nil, ""
+			return -1, ""
 		}
 
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			t.Fatal(err)
-			return nil, ""
+			return -1, ""
 		}
 
 		respBody, err := io.ReadAll(resp.Body)
 		if err != nil {
 			t.Fatal(err)
-			return nil, ""
+			return -1, ""
 		}
 		defer resp.Body.Close()
 
-		return resp, string(respBody)
+		return resp.StatusCode, string(respBody)
 	}
 
 	if _, body := testRequest(t, ts, "GET", "/value/gauge/test", nil); body != "1.3" {
 		t.Errorf("expected 1.3, got %s", body)
 	}
 
-	if resp, _ := testRequest(t, ts, "POST", "/update/gauge/test/1.3", nil); resp.StatusCode != http.StatusOK {
-		t.Errorf("expected status code 200, got %d", resp.StatusCode)
+	if respCode, _ := testRequest(t, ts, "POST", "/update/gauge/test/1.3", nil); respCode != http.StatusOK {
+		t.Errorf("expected status code 200, got %d", respCode)
 	}
 
-	if resp, _ := testRequest(t, ts, "POST", "/update/gauge/test/invalid_value", nil); resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status code 400, got %d", resp.StatusCode)
+	if respCode, _ := testRequest(t, ts, "POST", "/update/gauge/test/invalid_value", nil); respCode != http.StatusBadRequest {
+		t.Errorf("expected status code 400, got %d", respCode)
 	}
 
-	if resp, _ := testRequest(t, ts, "POST", "/update/invalid_type/test/1", nil); resp.StatusCode != http.StatusBadRequest {
-		t.Errorf("expected status code 400, got %d", resp.StatusCode)
+	if respCode, _ := testRequest(t, ts, "POST", "/update/invalid_type/test/1", nil); respCode != http.StatusBadRequest {
+		t.Errorf("expected status code 400, got %d", respCode)
 	}
 }
